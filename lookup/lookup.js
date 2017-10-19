eab738733fc5e0dab051b09594d2eac63a05b3e8
DavidHaas <cavoom@me.com>
2017-10-18 21:46:55 -0400

    gets the PersonID and saves json fle

--- lookup/lookup.js
+++ lookup/lookup.js
@@ -1,4 +1,4 @@
-// This is a TEST to convert ASH XML to JSON and Lookup
+// This script ADDS the PERSON ID to the SESSION Record
 
 //https://ash.confex.com/ash/2017/sync.cgi/Freeman_Session_Listing.xml
 //https://ash.confex.com/ash/2017/sync.cgi/Freeman_Paper_Listing.xml
@@ -13,31 +13,51 @@
 //var descriptions = require('./descriptions.js');
 
 var sessions = require('./codebeautify_SESSIONS.js');
-var theId = [11086,11085, 11084,11083,11082];
+var roles = require('./codebeautify_ROLE.js');
+var jsonfile = require('jsonfile'); // saves the object created
+var file = './data.json'; // saves the result to data.json
 
-attachIdToSession((response)=>{
-    console.log(response);
+
+attachPersonIdToSession((response)=>{
+    //console.log('combined array length: ', response.length);
+    //console.log('session array length: ', sessions.length);
+    //console.log('role array length', roles.length);
+    //console.log(response[590]);
+   
+// *** NEXT UP - BEFORE WE SAVE, have another callback that now gets the first name
+// last name and then as it's callback, it saves the file!
+// ********************************************************************************
+
+   // Save Routine
+    jsonfile.writeFile(file, response, function (err) {
+        if(response){
+            console.log('success');
+        } else {
+        console.error(err) }
+    })
+
 });
 
 
 // **********************************************************************************
 
-function attachIdToSession(callback){
+function attachPersonIdToSession(callback){
 
 var combinedObject = {};
 var resultingArray = [];
 
-theId.forEach(function(Id){
+roles.forEach(function(role){
 
     sessions.forEach(function(session) {
 
-    if(session.Key_SessionID == Id){
+    if(session.Key_SessionID == role.Entryid){
         // Now save this as a JSON object array
         combinedObject = {
-            "sessionId":Id,
+            "sessionId":session.Key_SessionID,
             "sessionTitle": session.SessionTitle,
             "sessionStartTime":session.StartTime,
-            "sessionEndTime": session.EndTime
+            "sessionEndTime": session.EndTime,
+            "sessionPerson": role.PersonID
         }
         resultingArray.push(combinedObject);
         //console.log(resultingArray.length);
