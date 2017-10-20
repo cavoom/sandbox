e3002d004772e06609d053de1e5478140eb4b6be
DavidHaas <cavoom@me.com>
2017-10-19 21:05:33 -0400

    assigns a session ID to each person ID

--- lookup/lookup.js
+++ lookup/lookup.js
@@ -1,28 +1,12 @@
 // This script ADDS the PERSON ID to the SESSION Record
 
-//https://ash.confex.com/ash/2017/sync.cgi/Freeman_Session_Listing.xml
-//https://ash.confex.com/ash/2017/sync.cgi/Freeman_Paper_Listing.xml
-//https://ash.confex.com/ash/2017/sync.cgi/Freeman_Person.xml
-//https://ash.confex.com/ash/2017/sync.cgi/Freeman_Role.xml
-//https://ash.confex.com/ash/2017/sync.cgi/Freeman_RoleAddress.xml
-//https://ash.confex.com/ash/2017/sync.cgi/Freeman_COI_PersonBased.xml
-// I converted the above XMLs to JSON with this service
-// https://codebeautify.org/xmltojson
-
-//var sessions = require('./sessions.js');
-//var descriptions = require('./descriptions.js');
-
 var sessions = require('./codebeautify_SESSIONS.js');
 var roles = require('./codebeautify_ROLE.js');
 var jsonfile = require('jsonfile'); // saves the object created
 var file = './data.json'; // saves the result to data.json
 
-
+// For each Person ID, assign a Session
 attachPersonIdToSession((response)=>{
-    //console.log('combined array length: ', response.length);
-    //console.log('session array length: ', sessions.length);
-    //console.log('role array length', roles.length);
-    //console.log(response[590]);
    
 // *** NEXT UP - BEFORE WE SAVE, have another callback that now gets the first name
 // last name and then as it's callback, it saves the file!
@@ -71,3 +55,52 @@ roles.forEach(function(role){
 callback(resultingArray);
 
 }
+
+// **********************************************************************************
+
+function attachNamesToMasterFile(){
+
+    var masterFile = require('./data.json');
+    console.log(masterFile[0]);
+
+
+// var combinedObject = {};
+// var resultingArray = [];
+
+// roles.forEach(function(role){
+
+//     sessions.forEach(function(session) {
+
+//     if(session.Key_SessionID == role.Entryid){
+//         // Now save this as a JSON object array
+//         combinedObject = {
+//             "sessionId":session.Key_SessionID,
+//             "sessionTitle": session.SessionTitle,
+//             "sessionStartTime":session.StartTime,
+//             "sessionEndTime": session.EndTime,
+//             "sessionPerson": role.PersonID
+//         }
+//         resultingArray.push(combinedObject);
+//         //console.log(resultingArray.length);
+//         //console.log(combinedObject);
+        
+        
+//         }
+//     });
+// });
+
+// callback(resultingArray);
+
+}
+
+
+// *************************************************************************************
+
+//https://ash.confex.com/ash/2017/sync.cgi/Freeman_Session_Listing.xml
+//https://ash.confex.com/ash/2017/sync.cgi/Freeman_Paper_Listing.xml
+//https://ash.confex.com/ash/2017/sync.cgi/Freeman_Person.xml
+//https://ash.confex.com/ash/2017/sync.cgi/Freeman_Role.xml
+//https://ash.confex.com/ash/2017/sync.cgi/Freeman_RoleAddress.xml
+//https://ash.confex.com/ash/2017/sync.cgi/Freeman_COI_PersonBased.xml
+// I converted the above XMLs to JSON with this service
+// https://codebeautify.org/xmltojson
