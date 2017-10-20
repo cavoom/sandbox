541b7d5afa09a341235d5a2faf8a7ea5d8e5eb9d
DavidHaas <cavoom@me.com>
2017-10-19 22:25:10 -0400

    works! creates a record with everything that we need!

---
+++ lookup/displayResults.js
@@ -0,0 +1,6 @@
+var results = require('./data.json');
+console.log(results.length);
+console.log(results[0]);
+console.log(results[250]);
+console.log(results[501]);
+
--- lookup/lookup.js
+++ lookup/lookup.js
@@ -2,29 +2,28 @@
 
 var sessions = require('./codebeautify_SESSIONS.js');
 var roles = require('./codebeautify_ROLE.js');
+var person = require('./codebeautify_PERSON.js');
 var jsonfile = require('jsonfile'); // saves the object created
-var file = './data.json'; // saves the result to data.json
-
+var masterFile = require('./data.json'); // this is the file that we created in step 1
+var file = './data.json'; // this is where I'm saving to
 // For each Person ID, assign a Session
 attachPersonIdToSession((response)=>{
    
-// *** NEXT UP - BEFORE WE SAVE, have another callback that now gets the first name
-// last name and then as it's callback, it saves the file!
-// ********************************************************************************
-
-   // Save Routine
-    jsonfile.writeFile(file, response, function (err) {
-        if(response){
-            console.log('success');
-        } else {
-        console.error(err) }
-    })
+    attachNamesToMasterFile((response2)=>{
+        // Save Routine
+            jsonfile.writeFile(file, response2, function (err) {
+            if(response){
+                console.log('success');
+            } else {
+            console.error(err) }
+        })
+        
+    });
 
 });
 
 
 // **********************************************************************************
-
 function attachPersonIdToSession(callback){
 
 var combinedObject = {};
@@ -57,39 +56,28 @@ callback(resultingArray);
 }
 
 // **********************************************************************************
+function attachNamesToMasterFile(callback){
+    
+    var combinedObject = {};
+    var resultingArray = [];
 
-function attachNamesToMasterFile(){
+    masterFile.forEach(function(master){
 
-    var masterFile = require('./data.json');
-    console.log(masterFile[0]);
+    person.forEach(function(peep) {
 
-
-// var combinedObject = {};
-// var resultingArray = [];
-
-// roles.forEach(function(role){
-
-//     sessions.forEach(function(session) {
-
-//     if(session.Key_SessionID == role.Entryid){
-//         // Now save this as a JSON object array
-//         combinedObject = {
-//             "sessionId":session.Key_SessionID,
-//             "sessionTitle": session.SessionTitle,
-//             "sessionStartTime":session.StartTime,
-//             "sessionEndTime": session.EndTime,
-//             "sessionPerson": role.PersonID
-//         }
-//         resultingArray.push(combinedObject);
-//         //console.log(resultingArray.length);
-//         //console.log(combinedObject);
+    if(master.sessionPerson == peep.Key_PersonID){
+        // Now save this as a JSON object array
+        master.firstName = peep.FirstName;
+        master.lastName = peep.LastName;
+        //console.log(resultingArray.length);
+        //console.log(combinedObject);
         
         
-//         }
-//     });
-// });
+        }
+    });
+});
 
-// callback(resultingArray);
+callback(masterFile);
 
 }
 
@@ -104,3 +92,5 @@ function attachNamesToMasterFile(){
 //https://ash.confex.com/ash/2017/sync.cgi/Freeman_COI_PersonBased.xml
 // I converted the above XMLs to JSON with this service
 // https://codebeautify.org/xmltojson
+
+
