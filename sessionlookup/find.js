45978146169dbf4fde69983504b0608da3684e0f
DavidHaas <cavoom@me.com>
2017-10-26 19:51:57 -0400

    works with remove from array

--- sessionlookup/find.js
+++ sessionlookup/find.js
@@ -1,32 +1,110 @@
 //SETUP TO FIND STUFF
 
+//STOPPED HERE
+// THEN GO TO THE ALEXA SIDE AND SEE IF WE CAN SEARCH BY KEYWORDS
+// AND FIND THOSE THAT DON'T HAVE IDENTIFIED KEYWORDS (SHOULD WORK)
+
 var allData = require('./ashData.json');
 // console.log(allData.length); // 591 records
 
 var i = 0; // counter for while loop
 var testSpeaker = "Seth Corey";
 var testSession = "Congenital and Acquired Neutropenia";
-var theKeyword = "Bone";
+var theKeyword = "Boned";
 var timeNow = new Date();
-var tempArray = [];
+//var tempArray = [];
 
 taxonomy((resultingStuff)=>{
-
     //console.log(resultingStuff);
     console.log('i found ', resultingStuff.length, 'items that match your search.');
-    //console.log(resultingStuff);
+    
+    if(resultingStuff.length > 0) {
     removem(resultingStuff, (uniques)=>{
-    console.log('I found ', uniques.length,' items that are unique.');
+        console.log('I found ', uniques.length,' items that are unique.');
 
         playback(uniques, (message)=>{
             console.log(message);
         })
     
-    
-
-})
+        })
+    }
 
 });
+
+
+// FIND A SESSION WITH A TAXONOMY KEYWORD
+function taxonomy(callback){
+    i-0;
+    var searchResults = [];
+    while (i < allData.length){
+        var title = allData[i].sessionTitle;
+        var startTime = allData[i].sessionStartTime;
+        startTime = new Date(startTime);
+        var endTime = allData[i].sessionEndTime;
+        endTime = new Date(endTime);
+
+        if(title.includes(theKeyword) && timeNow >= startTime && timeNow < endTime){
+            //console.log(allData[i].sessionId+"-"+allData[i].sessionTitle);
+            searchResults.push(allData[i]);
+        }
+
+    i++;
+
+}
+
+callback(searchResults);
+
+}
+
+// RETURN UNIQUE OBJECT ARRAY
+function removem(resultz, callback){
+    var uniques = [resultz[0]];
+    if(resultz.length > 1){
+    var numberRecords = resultz.length;
+    //console.log('i am in remove em ', numberRecords);
+    //console.log(resultz[0].sessionId);
+    i=1;
+
+    while (i < numberRecords){
+
+        if(resultz[i].sessionId != resultz[i-1].sessionId){
+            // this pushes only uniques to the array
+            uniques.push(resultz[i]);
+            //console.log(resultz[i].sessionId);
+        }
+            i++;
+    }
+}
+    callback(uniques);
+}
+
+
+// ORDER BY TIME
+// PLAY UP THE FIRST ONE
+// REMOVE FROM ARRAY
+// IF THERE ARE MORE LEFT, SAY NEXT TO HEAR NEXT
+
+// Playback the results
+function playback(uniques,callback){
+    //var list = "the stuff is " + uniques[0].sessionId + "," + uniques[1].sessionId + "," + uniques[2].sessionId;
+    if(uniques.length >1){
+        
+        //console.log('here is the list: ', list);
+        console.log('I found '+ uniques.length + ' sessions that match your search. The first one is '+ uniques[0].sessionTitle );
+        uniques.shift();
+        //console.log('the next one is ' + uniques[0].sessionTitle);
+        //console.log('there are now ', uniques.length, 'records.');
+    } else {
+        console.log('I found 1 session and it is ',uniques[0].sessionTitle);
+        //console.log('there are now ', uniques.length, 'records.');
+        uniques.shift();
+        //console.log('there are now: ', uniques.length);
+   
+    }
+
+    callback('all done');
+    // send it to session.attributes to save the remaining items!
+}
 
 // FIND A SPEAKER
 // while (i < allData.length){
@@ -41,79 +119,6 @@ taxonomy((resultingStuff)=>{
 //     i++;
 
 // }
-
-// FIND A SESSION WITH A TAXONOMY KEYWORD
-function taxonomy(callback){
-i-0;
-while (i < allData.length){
-
-    var title = allData[i].sessionTitle;
-    var startTime = allData[i].sessionStartTime;
-    startTime = new Date(startTime);
-    var endTime = allData[i].sessionEndTime;
-    endTime = new Date(endTime);
-
-    if(title.includes(theKeyword) && timeNow >= startTime && timeNow < endTime){
-
-        //console.log(allData[i].sessionId, "-", allData[i].sessionTitle);
-        tempArray.push(allData[i]);
-
-    }
-
-    i++;
-
-}
-//console.log('length of array: ',tempArray.length);
-callback(tempArray);
-
-}
-
-// RETURN UNIQUE OBJECT ARRAY
-function removem(resultz, callback){
-    var numberRecords = resultz.length;
-    //console.log('i am in remove em ', numberRecords);
-    var uniques = [resultz[0]];
-    console.log(resultz[0].sessionId);
-    i=1;
-
-    while (i < numberRecords){
-
-        if(resultz[i].sessionId != resultz[i-1].sessionId){
-            // this pushes only uniques to the array
-            uniques.push(resultz[i]);
-            console.log(resultz[i].sessionId);
-        }
-            i++;
-    }
-
-
-    
-    callback(uniques);
-}
-
-// ORDER BY TIME
-// PLAY UP THE FIRST ONE
-// REMOVE FROM ARRAY
-// IF THERE ARE MORE LEFT, SAY NEXT TO HEAR NEXT
-
-
-function playback(uniques,callback){
-    var list = "the stuff is " + uniques[0].sessionId + "," + uniques[1].sessionId + "," + uniques[2].sessionId;
-    if(uniques.length <= 3){
-        
-        console.log('here is the list: ', list);
-        console.log('I found '+ uniques.length + '. Here are the first 3: '+ list);
-    } else {
-        console.log('I found the BIG '+ uniques.length + '. Here are the first 3: '+ list);
-   
-    }
-
-    callback('all done');
-}
-
-
-// THEN GO TO THE ALEXA SIDE AND SEE IF WE CAN SEARCH BY KEYWORDS
-// AND FIND THOSE THAT DON'T HAVE IDENTIFIED KEYWORDS (SHOULD WORK)
 
 
 
